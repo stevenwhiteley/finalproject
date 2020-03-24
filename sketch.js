f0aaf6a2593145e71170d82b07d06af6189683f7
Steven <stevenwhiteley4@gmail.com>
2020-03-24 16:13:19 -0700

    final

--- sketch.js
+++ sketch.js
@@ -1,87 +1,153 @@
 
-
-let bertha;
 let ponyPic;
 let rainbowPic;
-let ponies = [];
-let howManyPonies = 200;
-let whichPony = 0;
 let img;
 let mouseBoxx;
 let mouseBoxy;
 let bg;
+let frames = 0
+let capture;
+let river;
+
 
 function setup(){
-  createCanvas(1210, 1000, WEBGL);
   ponyPic = loadModel('assets/TheRock2.obj');
+  tv = loadModel('assets/TV01.obj');
   img = loadImage('assets/TheRock2_skin_hi.jpg')
-  rainbowPic = loadImage('assets/rainbow.png');
   bg = loadImage('assets/TheRock2_teethUpper.jpg');
-  for (let i = 0; i < howManyPonies; i++){
-    ponies[i] = new Pony();
-  }
+  //river = createVideo('assets/river.mov')
+    createCanvas(1210, 1000, WEBGL);
+		   	image(bg, 0, 0, 1210, 1000);
+		   		background(0);
+//   vid.loop();
+//   vid.hide();
+//   button = createButton('play');
+//   button.mousePressed(toggleVid);
+//   }
+
+//   function toggleVid() {
+//   if (playing) {
+//     vid.pause();
+//     button.html('play');
+//   } else {
+//     vid.loop();
+//     button.html('pause');
+//   }
+//   playing = !playing;
+// }
 }
+
 function draw(){
-  background(bg);
-  image(rainbowPic, mouseX, mouseY, 50, 50);
-  noCursor();
-  for (let i = 0; i < howManyPonies; i++){
-    ponies[i].move();
-    ponies[i].display();
-  }
-  image(rainbowPic, mouseBoxx, mouseBoxy, 50, 50);
-}
 
-function mousePressed(){
-  mouseBoxx = mouseX;
-  mouseBoxy = mouseY;
-  ponies[whichPony].teleportPony(mouseX, mouseY);
-  ponies[whichPony].makePonyVisible();
-  whichPony++;
-  whichPony = whichPony % howManyPonies;
 
-}
-class Pony {
-  constructor(){
-    this.x = random(width);
-    this.y = random(height);
-    this.xspeed = random(0,1);
-    this.yspeed = random(0,1);
-    this.visible = false;
-  }
-  display(){
-    if (this.visible){
+	randomChord();
+  randomChord();
+
+	 frames++;
+  frames = frames % 360;
+  let rotationAmount = radians(frames);
+	push();
     texture(img);
     noStroke();
+    translate(0, -20, 0, 0);
     rotateX(1850);
-    rotateY(600);
-    scale(2);
-    model(ponyPic, this.x, this.y, 75, 75);
-    }
-  }
-  move(){
-    this.x = this.x + this.xspeed;
-    this.y = this.y + this.yspeed;
+    rotateY(rotationAmount * .9);
+   	scale(1, 1, 1);
+    //noStroke();
+      fill(random(255));
+  stroke(color(random(255), random(255), random(255)));
 
-    if ((this.x > width) || (this.x < 0)) {
-      this.xspeed = this.xspeed * -1;
-    }
+    model(ponyPic);
+    pop();
 
-    if ((this.y > height) || (this.y < 0)) {
-      this.yspeed = this.yspeed * -1;
-    }
+    	push();
+    texture(img);
+    noStroke();
+    translate(400, -20, 0, 0);
+    rotateX(1850);
+    rotateY(rotationAmount * .9);
+   	scale(1, 1, 1);
+    //noStroke();
+      fill(random(255));
+  stroke(color(random(255), random(255), random(255)));
 
-    if((this.x > mouseBoxx && this.x < (mouseBoxx + 50))&&(this.y > mouseBoxy && this.y < (mouseBoxy + 50))){
-      this.xspeed = this.xspeed + 50;
-      this.yspeed = this.yspeed + 50;
-    }
+    model(ponyPic);
+    pop();
 
-  }
-  teleportPony(xLoc, yLoc){
-    this.x = xLoc;
-    this.y = yLoc;
-  }
-  makePonyVisible(){
-    this.visible = true;
-  }
+     	push();
+    texture(img);
+    noStroke();
+    translate(-400, -20, 0, 0);
+    rotateX(1850);
+    rotateY(rotationAmount * .9);
+   	scale(1, 1, 1);
+    //noStroke();
+      fill(random(255));
+  stroke(color(random(255), random(255), random(255)));
+
+    model(ponyPic);
+    pop();
+
+
+function randomChord() {
+     frames++;
+  frames = frames % 360;
+  let rotationAmount = radians(frames);
+  push();
+  stroke(color(random(255), random(255), random(255)));
+  fill(random(255));
+  translate(0, -350, 20, 20);
+  rotateX(1900);
+  //rotateY(600);
+  rotateY(rotationAmount * .9);
+  scale(.9)
+  model(ponyPic);
+  pop();
+
+   push();
+  stroke(color(random(255), random(255), random(255)));
+  fill(random(255));
+  translate(0, 330, 20, 20);
+  rotateX(1800);
+  //rotateY(600);
+  scale(.9);
+    rotateY(rotationAmount * .9);
+  model(ponyPic);
+  pop();
+
+  push();
+  let angle1 = random(0, 2 * PI);
+  let xpos1 = -1500 + -1500 * cos(angle1);
+  let ypos1 = -1500 + -1500 * sin(angle1);
+
+  // find another random point on the circle
+  let angle2 = random(0, 2 * PI);
+  let xpos2 = 500 + -800 * cos(angle2);
+  let ypos2 = 500 + -800 * sin(angle2);
+  stroke(color(random(255), random(255), random(255)));
+  // draw a line between them
+  //fill(color(75, 75, 75));
+  fill(0);
+  rect(xpos1, ypos1, xpos2, ypos2);
+  pop();
+
+  // push();
+  // model(tv);
+  // texture(river);
+  // pop();
+
+
 }
+
+    // push();
+    // texture(img);
+    // noStroke();
+    // rotateX(1850);
+    // rotateY(600 * frameCount);
+   	// scale(random(10));
+    // translate(-250, -300, 95, 95);
+    // model(ponyPic);
+    // pop();
+
+    }
+  
